b5e7d88d6941a36ce41952635dd9c42d54faad0b
stormrageqb <stormrage.qb@gmail.com>
2022-10-22 14:16:41 -0500

    Add mouseover paint square functionality

--- src/script.js
+++ src/script.js
@@ -4,18 +4,27 @@ const gridContainer = document.querySelector(".grid-container");
 
 const sixteenGrid = () => {
   for (let i = 1; i <= 256; i++) {
-    const squareEl = document.createElement("div");
-    squareEl.classList.add("square");
-    gridContainer.appendChild(squareEl);
+    const rectEl = document.createElement("div");
+    rectEl.setAttribute("data-id", "box");
+    rectEl.classList.add("rect");
+    gridContainer.appendChild(rectEl);
   }
 };
 
 sixteenGrid();
 
+gridContainer.addEventListener("mouseover", function (e) {
+  const boxEl = e.target.closest("div");
+  if (!boxEl) return;
+  console.log(boxEl);
+  console.log("box!!");
+  boxEl.style.backgroundColor = "blue";
+});
+
 // const twentyFourGrid = () => {
 //   for (let i = 1; i <= 576; i++) {
 //     const squareEl = document.createElement("div");
-//     squareEl.classList.add("small-square");
+//     squareEl.classList.add("small-rect");
 // Also change grid template columns
 //     gridContainer.appendChild(squareEl);
 //   }
