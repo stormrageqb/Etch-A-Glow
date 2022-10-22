a2eefe6752ddaf20501db3b6c48592caf5459d88
stormrageqb <stormrage.qb@gmail.com>
2022-10-22 10:40:09 -0500

    Add basic layout and title of app

---
+++ src/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const gridContainer = document.querySelector(".grid-container");
+
+// const divEl = document.createElement("div");
+// divEl.classList.add("square");
+// gridContainer.appendChild(divEl);
