54c2753ab8186b04097df7567c8a618209da0d9b
stormrageqb <stormrage.qb@gmail.com>
2022-10-23 15:39:47 -0500

    Code cleanup and add color palette functionality - change cell color and grid gap color

--- src/script.js
+++ src/script.js
@@ -1,59 +1,54 @@
-"use strict";
+'use strict';
 
-const gridContainer = document.querySelector(".grid-container");
-const rectInp = document.querySelector(".rectangle-input");
-const gapInp = document.querySelector(".gap-input");
-const gridSizingForm = document.querySelector(".grid-sizing-form");
+const gridContainer = document.querySelector('.grid-container');
 
-// const sixteenGrid = () => {
-//   for (let i = 1; i <= 256; i++) {
-//     const rectEl = document.createElement("div");
-//     rectEl.setAttribute("data-id", "box");
-//     rectEl.classList.add("rect");
-//     gridContainer.appendChild(rectEl);
-//   }
-// };
+// Grid Sizing:
+const rectInp = document.querySelector('.rectangle-input');
+const gapInp = document.querySelector('.gap-input');
+const gridSizingForm = document.querySelector('.grid-sizing-form');
 
-// sixteenGrid();
+// Grid Coloring:
+const rectColor = document.querySelector('.cell-color-input');
+const gapColor = document.querySelector('.gap-color-input');
+const colorPaletteForm = document.querySelector('.grid-color-form');
+const defaultColor = '#ff143fca';
+let cellColor;
 
-gridContainer.addEventListener("mouseover", function (e) {
-  const boxEl = e.target.closest("div");
+gridContainer.addEventListener('mouseover', function (e) {
+  const boxEl = e.target.closest('div');
   if (!boxEl) return;
-  //   console.log(boxEl);
-  //   console.log("box!!");
-  boxEl.style.backgroundColor = "blue";
+  boxEl.style.backgroundColor = defaultColor;
+  boxEl.style.backgroundColor = `${cellColor}`;
 });
 
-gridSizingForm.addEventListener("input", (e) => {
+colorPaletteForm.addEventListener('input', function () {
+  cellColor = rectColor.value;
+  console.log(cellColor);
+});
+
+gridSizingForm.addEventListener('input', e => {
   e.preventDefault();
   const rectValue = +rectInp.value;
   const gapValue = +gapInp.value;
   console.log(gapValue);
   const numOfRect = rectValue * rectValue;
   console.log(rectValue);
-  gridContainer.innerHTML = "";
+  gridContainer.innerHTML = '';
   for (let i = 1; i <= numOfRect; i++) {
     // gridContainer.style.gridTemplateColumns = `repeat(${formValue}, minmax(0.5rem, 1fr)`;
     if (rectValue >= 101) return;
     if (gapValue >= 6) return;
     gridContainer.style.gridTemplateColumns = `repeat(${rectValue}, minmax(0.1rem, 1fr)`;
     gridContainer.style.gap = `${gapValue / 10}rem`;
-    const rectEl = document.createElement("div");
-    rectEl.setAttribute("data-id", "box");
-    rectEl.classList.add("rect");
+    const rectEl = document.createElement('div');
+    rectEl.setAttribute('data-id', 'box');
+    rectEl.classList.add('rect');
     gridContainer.appendChild(rectEl);
   }
 });
 
-// console.log(getFormValue());
-
-// const twentyFourGrid = () => {
-//   for (let i = 1; i <= 576; i++) {
-//     const squareEl = document.createElement("div");
-//     squareEl.classList.add("small-rect");
-// Also change grid template columns
-//     gridContainer.appendChild(squareEl);
-//   }
-// };
-
-// twentyFourGrid();
+colorPaletteForm.addEventListener('input', function () {
+  const gapColorValue = gapColor.value;
+  // console.log(gapColorValue);
+  gridContainer.style.backgroundColor = gapColorValue;
+});
