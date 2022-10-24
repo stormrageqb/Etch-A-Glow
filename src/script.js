2c2d7acb3fc4149317ba46b1db070cc571adcf11
stormrageqb <stormrage.qb@gmail.com>
2022-10-24 15:07:43 -0500

    Successful implementation of rainbow toggle - change each square color on mouseover

--- src/script.js
+++ src/script.js
@@ -19,17 +19,25 @@ const innerGlow = document.querySelector('.inner-glow-input');
 const outerGlow = document.querySelector('.outer-glow-input');
 const innerGlowForm = document.querySelector('.inner-glow-form');
 const outerGlowForm = document.querySelector('.outer-glow-form');
+const rainbowCheckbox = document.querySelector('input[name=checkbox-rainbow]');
 const disableGlowCheckbox = document.querySelector('input[name=checkbox-glow]');
+let rainbowColor;
 let defaultShadow;
 let innerGlowValue;
 let outerGlowValue;
 let toggle;
+let toggleRainbow;
 
 gridContainer.addEventListener('mouseover', function (e) {
   const boxEl = e.target.closest('div');
   if (!boxEl) return;
   boxEl.style.backgroundColor = defaultColor;
   boxEl.style.backgroundColor = `${cellColor}`;
+
+  if (toggleRainbow) {
+    boxEl.style.backgroundColor = getRandomColor(255);
+  }
+
   if (toggle) {
     boxEl.style.boxShadow = `inset 0 0 1.2rem ${innerGlowValue}`;
   } else {
@@ -59,9 +67,18 @@ const changeGlow = function () {
 
 changeGlow();
 
+rainbowCheckbox.addEventListener('change', function () {
+  if (this.checked) {
+    // rainbowColor = getRandomColor(255);
+    // console.log(rainbowColor);
+    toggleRainbow = true;
+  } else {
+    toggleRainbow = false;
+  }
+});
+
 disableGlowCheckbox.addEventListener('change', function () {
   if (this.checked) {
-    console.log('checked!!');
     defaultShadow = `0 3px 10px rgb(0 0 0 / 0.2)`;
   } else {
     defaultShadow = changeGlow();
@@ -94,3 +111,21 @@ colorPaletteForm.addEventListener('input', function () {
   // console.log(gapColorValue);
   gridContainer.style.backgroundColor = gapColorValue;
 });
+
+const getRandomColor = function (num) {
+  const arr = [];
+  // const possibilties = ['a','b','c','d']
+  // const pound = '#';
+  // arr.push(pound);
+  // console.log(arr);
+  const firstRandomNum = Math.floor(Math.random() * num);
+  const secondRandomNum = Math.floor(Math.random() * num);
+  const thirdRandomNum = Math.floor(Math.random() * num);
+  console.log(firstRandomNum);
+  console.log(secondRandomNum);
+  console.log(thirdRandomNum);
+  return `rgb(${firstRandomNum}, ${secondRandomNum}, ${thirdRandomNum})`;
+};
+
+getRandomColor(255);
+console.log(getRandomColor(255));
